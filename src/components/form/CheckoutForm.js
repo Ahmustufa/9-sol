01d4bf49ca2253d1b173f37b2a3ef569e950ee73
MustufaEforte <mustafa@eforte.net>
2023-02-16 17:17:56 +0500

    stripe ui payment-elements

--- src/components/form/CheckoutForm.js
+++ src/components/form/CheckoutForm.js
@@ -1,23 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   useStripe,
   useElements,
   PaymentElement,
+  CardElement,
+  LinkAuthenticationElement,
+  PayButtonElement,
 } from "@stripe/react-stripe-js";
+import BlackButton from "../buttons/BlackButton";
+import Text from "../Typography/Text";
+import { Box } from "@mui/material";
 
-function CheckoutForm(paymentIntent) {
+function CheckoutForm(props) {
+  // const { paymentIntent } = props;
   const stripe = useStripe();
   const elements = useElements();
-  if (!stripe || !elements) {
-    // Stripe.js has not yet loaded.
-    // Make sure to disable form submission until Stripe.js has loaded.
-    return;
-  }
+
+  const handleSubmit = async (event) => {
+    // event.preventDefault();
+    // const { error, paymentIntent } = await stripe.confirmCardPayment(
+    //   props.paymentIntent,
+    //   {
+    //     payment_method: {
+    //       card: elements.getElement(PaymentElement),
+    //     },
+    //   }
+    // );
+    // console.log(paymentIntent);
+    // if (error) {
+    //   console.log(error.message);
+    // } else if (paymentIntent.status === "succeeded") {
+    //   console.log("Payment succeeded!");
+    // }
+  };
+  // useEffect(() => {
+  //   if (elements) {
+  //     const paymentElement = elements.getElement(PaymentElement);
+  //     paymentElement.on("change", (event) => {
+  //       if (event.error) {
+  //         console.log("Error:", event.error.message);
+  //         // handle error here
+  //       }
+  //     });
+  //   }
+  // }, [elements]);
+  // const paymentElementOptions = {
+  //   layout: "tabs",
+  // };
+
   return (
-    <form>
-      <PaymentElement />
-      <button>Submit</button>
-    </form>
+    <Box component="form" onSubmit={handleSubmit}>
+      {props.paymentIntent && (
+        <div >
+          <Text variant="h4" textAlign="center">
+            Payment details
+          </Text>
+          <PaymentElement />
+          <BlackButton disabled={!stripe}>Submit</BlackButton>
+        </div>
+      )}
+    </Box>
   );
 }
 
--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -3,57 +3,67 @@ import CheckoutForm from "@/components/form/CheckoutForm";
 import { Elements } from "@stripe/react-stripe-js";
 import styled from "styled-components";
 import { loadStripe } from "@stripe/stripe-js";
+import "@stripe/stripe-js";
 import axios from "axios";
+import { Grid } from "@mui/material";
+import PropagateLoader from 'react-spinners/PropagateLoader' 
+const stripe = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}`);
 
-const stripe = loadStripe(
-  "pk_test_51LrmO2ExvHTSaBsJ8bJ3X15qUR01gIow6sjcQUdB4U0avz5jAJ1IMDacq1vakBK7fwSCZT6sM9qrYJkfL6IWZick00cg24G0kF"
-);
 function CheckoutPage() {
   const [clientSecret, setClientSecret] = useState("");
   const [paymentIntent, setPaymentIntent] = useState("");
   console.log("stripePromise", stripe);
+
+  const getPaymentIntent = async () => {
+    try {
+      const response = await axios.post("api/stripe_intent", { amount: 3000 });
+      const { client_secret, id } = response.data;
+      setPaymentIntent(id);
+      setClientSecret(client_secret);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+  useEffect(() => {
+    getPaymentIntent();
+  }, []);
+  console.log("clientSecret", clientSecret);
   const options = {
     clientSecret,
     appearance: {
       theme: "stripe",
       labels: "floating",
+      variables: {
+        colorPrimary: "#0570de",
+        colorBackground: "#ffffff",
+        colorText: "#30313d",
+        colorDanger: "#df1b41",
+        fontFamily: "Ideal Sans, system-ui, sans-serif",
+        spacingUnit: "2px",
+        borderRadius: "4px",
+      },
     },
   };
-  const getPaymentIntent = async () => {
-    try {
-      const response = await axios.post("api/stripe_intent", { amount: 3000 });
-      console.log(response.data.client_secret);
-    } catch (error) {}
-  };
-  useEffect(() => {
-    getPaymentIntent();
-    // Create PaymentIntent as soon as the page loads using our local API
-    // fetch("api/stripe_intent", {
-    //   method: "POST",
-    //   headers: { "Content-Type": "application/json" },
-    //   body: JSON.stringify({
-    //     amount: 30000,
-    //     payment_intent_id: "",
-    //   }),
-    // })
-    //   .then((res) => res.json())
-    //   .then((data) => setClientSecret(data.client_secret));
-  }, [clientSecret]);
-  console.log("clientSecret", clientSecret);
+
+  // console.log("options", options);
   return (
-    <StyledSection>
-      <Elements stripe={stripe} options={options}>
-        <CheckoutForm paymentIntent={paymentIntent} />
-      </Elements>
-    </StyledSection>
+    <Grid
+      container
+      height={"100vh"}
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Grid xs={6} mx={2}>
+        {stripe && clientSecret ? (
+          <Elements options={options} stripe={stripe}>
+            <CheckoutForm paymentIntent={paymentIntent} />
+          </Elements>
+        ) : (
+          <PropagateLoader color="#aa076b" />
+        )}
+      </Grid>
+    </Grid>
   );
 }
 
 export default CheckoutPage;
-
-const StyledSection = styled.section`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
