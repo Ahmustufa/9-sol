74b5ab0aa1c1a2dec69d22c6e15a3f46cc27b085
mustafaEforte <mustafa@eforte.net>
2023-11-05 06:11:01 +0500

    updated sitemap xml

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -2,7 +2,7 @@ const baseUrl = "https://www.9solutions.co";
 
 function generateSiteMap(urls) {
   return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
      ${urls
        .map((item) => {
          return `
@@ -27,32 +27,32 @@ export async function getServerSideProps({ res }) {
   const urls = [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       priority: 1.0,
     },
     {
       url: `${baseUrl}/aboutus`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       priority: 0.8,
     },
     {
       url: `${baseUrl}/services`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       priority: 0.8,
     },
     {
       url: `${baseUrl}/contactus`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       priority: 0.8,
     },
     {
       url: `${baseUrl}/term-of-use`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       priority: 0.8,
     },
     {
       url: `${baseUrl}/privacy-policy`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       priority: 0.8,
     },
   ];
